ab68cf59b967999982221de7b063992ae6551c7c
akarag05 <karageorgisa1997@gmail.com>
2025-11-05 18:58:08 +0200

    [AK] conversion to TS

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import fs from "fs";
+
+const getText = (path: string) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf8", (error: any, data: string) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+const start = async () => {
+  try {
+    const first = await getText("./content/first.txt");
+    console.log(first);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+// getText('./content/first.txt')
+//     .then(result => console.log(result))
+//     .catch((error) => {console.log(error)})
